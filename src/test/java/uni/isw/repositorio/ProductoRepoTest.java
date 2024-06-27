8e7311292d8eeaa7c551492973482bd50c89ba72
Your Name <you@example.com>
2024-06-27 00:46:59 -0500

    Dockerfile actualizacion

--- src/test/java/uni/isw/repositorio/ProductoRepoTest.java
+++ src/test/java/uni/isw/repositorio/ProductoRepoTest.java
@@ -20,7 +20,7 @@ public class ProductoRepoTest {
     @Autowired
     private ProductoRepo productoRepo;
     
-     @BeforeEach
+    @BeforeEach
     public void setUp() {
         Producto producto1 = Producto.builder()
                 .nombre("Rose")
